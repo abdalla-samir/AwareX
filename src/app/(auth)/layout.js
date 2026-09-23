7abb92afd3708111b5686c06a3f6e922f21e2e1c
abdalla-samir <abdallasamir860@gmail.com>
2026-09-23 21:25:16 +0300

    Add new changes

---
+++ src/app/(auth)/layout.js
@@ -0,0 +1,7 @@
+export default function layout({ children }) {
+    return (
+        <div className="bg-surface min-h-screen flex items-center justify-center">
+            {children}
+        </div>
+    );
+}
---
+++ src/components/auth/FormCard.js
@@ -0,0 +1,3 @@
+export default function FormCard({ children }) {
+    return <div className="w-2/3">{children}</div>;
+}
